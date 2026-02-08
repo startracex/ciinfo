bf67bde6271438e2dcc1caec9ca5944bcff40641
Shiro Wang <startraceebr@foxmail.com>
2026-02-09 07:06:38 +0800

    feat: add ciinfo package

---
+++ ciinfo.go
@@ -0,0 +1,95 @@
+package ciinfo
+
+import (
+	"os"
+	"strings"
+	"sync"
+
+	"github.com/startracex/ciinfo/vendors"
+)
+
+var (
+	infoOnce      sync.Once
+	infoOnceValue *Info
+)
+
+type Info struct {
+	IsPR    bool
+	IsCI    bool
+	ID      string
+	Name    string
+	Vendors map[string]bool
+}
+
+func EnvironMap(env []string) map[string]string {
+	out := make(map[string]string, len(env))
+	for _, envString := range env {
+		if key, value, ok := strings.Cut(envString, "="); ok {
+			out[key] = value
+		}
+	}
+	return out
+}
+
+func GetInfo() *Info {
+	infoOnce.Do(func() {
+		infoOnceValue = GetInfoFrom(EnvironMap(os.Environ()), vendors.VendorsAll)
+	})
+	return infoOnceValue
+}
+
+func GetInfoFrom(env map[string]string, vendors []vendors.Vendor) *Info {
+	if isExplicitlyFalseLike(env["CI"]) {
+		return nil
+	}
+
+	info := Info{
+		Vendors: make(map[string]bool, 2),
+	}
+
+	for _, vendor := range vendors {
+		if !vendor.Env.Match(env) {
+			continue
+		}
+
+		info.Vendors[vendor.Constant] = true
+		info.IsCI = true
+		info.Name = vendor.Name
+		info.ID = vendor.Constant
+
+		if vendor.PR != nil {
+			info.IsPR = vendor.PR.Match(env)
+		}
+	}
+
+	if !info.IsCI {
+		info.IsCI = fromCommonKeys(env)
+	}
+
+	return &info
+}
+
+var commonKeys = []string{
+	"BUILD_ID",
+	"BUILD_NUMBER",
+	"CI",
+	"CI_APP_ID",
+	"CI_BUILD_ID",
+	"CI_BUILD_NUMBER",
+	"CI_NAME",
+	"CONTINUOUS_INTEGRATION",
+	"RUN_ID",
+}
+
+func fromCommonKeys(env map[string]string) bool {
+	for _, k := range commonKeys {
+		if env[k] != "" {
+			return true
+		}
+	}
+	return false
+}
+
+func isExplicitlyFalseLike(s string) bool {
+	return s == "false" || s == "0"
+}
